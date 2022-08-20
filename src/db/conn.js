f2bc10ad5af2a677c2ea6b9818636faa69f455b1
unknown <jagannathdalei1236@gmail.com>
2022-08-20 21:24:27 +0530

    update

--- src/db/conn.js
+++ src/db/conn.js
@@ -2,7 +2,7 @@ const mongoose=require("mongoose");
 
 // mongoose.connect("mongodb://localhost:27017/youtubeRegistration")
 // mongoose.connect("mongodb+srv://jagannath1:Subhashree@1@cluster0.xvfn2nw.mongodb.net/?retryWrites=true&w=majority")
-mongoose.connect("mongodb+srv://jagannath1:<password>@cluster0.xvfn2nw.mongodb.net/test")
+mongoose.connect("mongodb+srv://jagannath1:Subhashree@1@cluster0.xvfn2nw.mongodb.net/test")
 .then(()=>{
     console.log("connection successful");
 }).catch((e)=>{
