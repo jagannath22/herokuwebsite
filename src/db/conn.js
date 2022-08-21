5d77b502b0456f196c99f9374f6f17eea0a31c7a
unknown <jagannathdalei1236@gmail.com>
2022-08-21 13:01:26 +0530

    update

--- src/db/conn.js
+++ src/db/conn.js
@@ -1,10 +1,10 @@
-const mongoose=require("mongoose");
+// const mongoose=require("mongoose");
 
-mongoose.connect("mongodb://localhost:27017/youtubeRegistration")
-// mongoose.connect("mongodb+srv://jagannath1:Subhashree@1@cluster0.xvfn2nw.mongodb.net/?retryWrites=true&w=majority")
+// //mongoose.connect("mongodb://localhost:27017/youtubeRegistration")
+// // mongoose.connect("mongodb+srv://jagannath1:Subhashree@1@cluster0.xvfn2nw.mongodb.net/?retryWrites=true&w=majority")
 // mongoose.connect("mongodb+srv://jagannath1:Subhashree@1@cluster0.xvfn2nw.mongodb.net/test")
-.then(()=>{
-    console.log("connection successful");
-}).catch((e)=>{
-    console.log("no connection");
-})
+// .then(()=>{
+//     console.log("connection successful");
+// }).catch((e)=>{
+//     console.log("no connection");
+// })
